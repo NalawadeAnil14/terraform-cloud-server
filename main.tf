2ad80882e450cc46fa52c6a2b1fa3139c439c14b
Anil Nalawade <nalawadeanil14@gmail.com>
2026-05-31 10:04:37 +0530

    Added suffix public in subnet id section

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "server1" {
   ami           = "ami-00e801948462f718a"
   instance_type = var.instance_type
-  subnet_id     = data.terraform_remote_state.vpc.outputs.public_subnet_ids["${var.az}"]
+  subnet_id     = data.terraform_remote_state.vpc.outputs.public_subnet_ids["${var.az}-public"]
 }
 
